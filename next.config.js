8bbe8974f63080b1699cdc180469f9f44d5feb33
Robby Barton <rbartonsc@gmail.com>
2023-04-19 11:16:37 -0400

    update packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 // next.config.js
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
   output: 'standalone',
-}
+})
