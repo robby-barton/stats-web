52a9758815af353d2e4e0b118a0b016dd1ba85f6
Robby Barton <rbartonsc@gmail.com>
2023-05-11 11:29:33 -0400

    use static image for error images (#52)

--- tests/components/teamName.test.tsx
+++ tests/components/teamName.test.tsx
@@ -1,13 +1,28 @@
+import { useContext } from "react";
+
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
 import TeamName from "@components/teamName";
-import { ThemeProvider } from "@components/themeProvider";
+import { ThemeContext, ThemeProvider } from "@components/themeProvider";
 import { ERROR_IMAGES } from "@lib/constants";
 
 beforeEach(() => {
 	document.body.dataset.theme = "light";
 });
+
+function TestTeamName({ team_id, name }: { team_id: number; name: string }) {
+	const { setColorMode } = useContext(ThemeContext);
+
+	return (
+		<div>
+			<button onClick={() => setColorMode("dark")} aria-label="dark">
+				Dark Mode
+			</button>
+			<TeamName team_id={team_id} name={name} />
+		</div>
+	);
+}
 
 describe("TeamName", () => {
 	it("errors to a dog img", async () => {
@@ -21,6 +36,21 @@ describe("TeamName", () => {
 
 		fireEvent.error(img);
 
-		expect(img.src).toContain(ERROR_IMAGES[team_id % ERROR_IMAGES.length]);
+		expect(img.src).toContain(ERROR_IMAGES[team_id % ERROR_IMAGES.length].src.replace("/", ""));
+	});
+
+	it("changes to dark", async () => {
+		render(
+			<ThemeProvider>
+				<TestTeamName team_id={2579} name="South Carolina" />
+			</ThemeProvider>
+		);
+		const img = (await screen.findByAltText("South Carolina")) as HTMLImageElement;
+
+		expect(img.src).not.toContain("dark");
+
+		fireEvent.click(screen.getByRole("button", { name: "dark" }));
+
+		expect(img.src).toContain("dark");
 	});
 });
